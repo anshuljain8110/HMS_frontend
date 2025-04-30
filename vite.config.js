ab6f142631ea5e541b998ebac4b9df9cb381d89c
Anshul Jain <anshuljain8110@gmail.com>
2025-04-30 12:28:03 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH 
+  base: process.env.VITE_BASE_PATH,
+  build: {
+    chunkSizeWarningLimit: 10000, // increases limit to 2000 KB (2 MB)
+  },
 })
